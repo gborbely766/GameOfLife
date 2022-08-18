5f045a5877c5b08a0f6fc12d27a5321ecea7686c
gborbely766 <gborbely766@gmail.com>
2022-08-18 09:02:45 +0200

    add Cell constructor

--- GameOfLife.ConsoleApp/Cell.cs
+++ GameOfLife.ConsoleApp/Cell.cs
@@ -11,6 +11,11 @@ namespace GameOfLife.ConsoleApp
         public int X { get; }
         public int Y { get; }
         public bool IsAlive { get; set; }
-
+        public Cell(int x, int y, bool isAlive)
+        {
+            X = x;
+            Y = y;
+            IsAlive = isAlive;
+        }
     }
 }
